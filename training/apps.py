c02aeb9fc158098a23b375d8e47f2001956b4eea
Wilson Benítez <wilben.1986@gmail.com>
2026-08-02 16:39:09 -0500

    UI: cambiar 'Formación' por 'Capacitación' en el panel de administración

--- training/apps.py
+++ training/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class TrainingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "training"
-    verbose_name = "Formación"
+    verbose_name = "Capacitación"
 
 from django.apps import AppConfig
